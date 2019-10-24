272148d8c58e2a9bac25a8debe50ef3791efc5b9
Zhen <zhx013@ucsd.edu>
2019-10-24 13:47:07 -0700

    resolve

---
+++ Assets/Script/barrier_movement_2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrier_movement_2 : MonoBehaviour
+{
+    public SphereCollider barrier;
+    private float count = 0;
+    private float position;
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        count += Time.deltaTime;
+        if (count > 14)
+        {
+            count -= 14;
+        }
+        if (count >= 0 && count <= 3.5)
+            position = -count - 47.5f;
+        else if (count > 3.5 && count <= 7)
+            position = -54.5f + count;
+        else if (count > 7 && count <= 10.5)
+            position = -54.5f + count;
+        else if (count > 10.5 && count <= 14)
+            position = -33.5f - count;
+        barrier.transform.localPosition = new Vector3(60f, 3f, position);
+    }
+}
