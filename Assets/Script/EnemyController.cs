f3b452760235ebca4f3b20fdc0023fc52b2d3b62
Chuhua Sun <chs090@ucsd.edu>
2019-11-01 16:22:31 -0700

    enemy update -- discarded
    discarded enemy update

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class EnemyController : MonoBehaviour
+{
+    public float check_radius = 10f;
+    Transform target;
+
+    public GameObject player;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<PlayerManager>().instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+        if(dist < check_radius){
+            agent.SetDestination(target.position);
+        }
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color  = Color.red;
+        Gizmos.DrawWireSphere(transform.position, check_radius);
+    }
+}
---
+++ Assets/Script/PlayerManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Singloeton
+        public PlayerManager instance;
+        void Awake()
+        {
+            instance = this;
+        }
+    #endregion
+
+
+    public GameObject player;
+    
+}
